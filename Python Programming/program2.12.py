99686c3b6295f9ddfd2ec06c1299f705149ee969
Tech-Shreyan <113615373+Tech-Shreyan@users.noreply.github.com>
2024-07-28 21:54:05 +0530

    Delete Python Programming

--- Python Programming/program2.12.py
+++
@@ -1,9 +0,0 @@
-
-def print_multiplication_table(num):
-    for i in range(1, 11):
-        print(f"{num} x {i} = {num * i}")
-
-
-number = int(input("Enter a number: "))
-
-print_multiplication_table(number)
--- Python Programming/program2.7.py
+++
@@ -1,9 +0,0 @@
-
-def reverse_string(s):
-    return s[::-1]
-input_string = input("Enter a string: ")
-
-reversed_string = reverse_string(input_string)
-
-
-print(f"The reversed string is: {reversed_string}")
