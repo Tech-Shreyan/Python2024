6e4bd597ae817b2dc2a05bbc3a443312a3f843a5
Tech-Shreyan <113615373+Tech-Shreyan@users.noreply.github.com>
2024-08-02 12:30:17 +0530

    Delete Python Programming directory

--- Python Programming/program2.py
+++
@@ -1,8 +0,0 @@
-
-def swap(a, b):
-    return b, a
-x = float(input("Enter the first number: "))
-y = float(input("Enter the second number: "))
-x, y = swap(x, y)
-print(f"The first number after swapping: {x}")
-print(f"The second number after swapping: {y}")
